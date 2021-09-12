70d940b47aaaf2d47b9f1bb6d016ae86e27b355e
favorcat <favorcat.me@gmail.com>
2021-09-12 21:47:56 +0900

    Bronze IV #2530
    인공지능 시계

---
+++ Bronze/Bronze IV/2530.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[]args){
+        Scanner scan = new Scanner(System.in);
+
+        int h = scan.nextInt();
+        int m = scan.nextInt();
+        int s = scan.nextInt();
+        int t = scan.nextInt();
+
+        s += t;
+        m += s/60;
+        s %= 60;
+        h += m/60;
+        m %= 60;
+        h %= 24;
+
+        System.out.println(h+" "+m+" "+s);
+    }
+}
