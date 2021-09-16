00716488278296fe3c9b647b87667d84cbd78d4c
favorcat <favorcat.me@gmail.com>
2021-09-16 22:23:37 +0900

    Bronze III #10817
    세 수

---
+++ Bronze/Bronze III/10817.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[]args){
+        Scanner scan = new Scanner(System.in);
+
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        int[] arr = {a,b,c};
+        Arrays.sort(arr);
+
+        System.out.println(arr[1]);
+    }
+}
