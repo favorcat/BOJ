fdd1385199fa08052daf95ca93d5d36185f48925
favorcat <favorcat.me@gmail.com>
2021-10-07 22:04:16 +0900

    Bronze II #2743
    단어 길이 재기

---
+++ Bronze/Bronze II/2743.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[]args){
+        Scanner scan = new Scanner(System.in);
+
+        String s = scan.next();
+        System.out.println(s.length());
+    }
+}
