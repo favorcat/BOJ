2da7352908cec225d889cb52c5c6ed453a237747
favorcat <favorcat.me@gmail.com>
2021-10-10 15:01:18 +0900

    Bronze IV #15680
    연세대학교

---
+++ Bronze/Bronze IV/15680.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false); 
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    if (n == 0) cout << "YONSEI";
+    else cout << "Leading the Way to the Future";
+}
