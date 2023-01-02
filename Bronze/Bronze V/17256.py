72e2af20fa858569ffabc9abc5497d519c03088e
Eunyeong Ko <favorcat.me@gmail.com>
2023-01-02 12:29:09 +0900

    Bronze V #17256 달달함이 넘쳐흘러

---
+++ Bronze/Bronze V/17256.py
@@ -0,0 +1,10 @@
+a = list(map(int,input().split()))
+c = list(map(int,input().split()))
+b = []
+
+b.append(c[0]-a[2])
+b.append(c[1]//a[1])
+b.append(c[2]-a[0])
+
+for res in b:
+    print(res, end=" ")
