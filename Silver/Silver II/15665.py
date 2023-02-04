2238303ac7355e5bfdf7a2862801c82186424439
Eunyeong Ko <favorcat.me@gmail.com>
2023-02-04 22:11:18 +0900

    Silver II #15665 N과 M (11)
    https://www.acmicpc.net/problem/15665
    https://favorcat.tistory.com/60

---
+++ Silver/Silver II/15665.py
@@ -0,0 +1,18 @@
+n,m = map(int,input().split())
+a = list(map(int,input().split()))
+a.sort()
+ans = []
+
+def backtracking(depth):
+  if depth == m:
+    print(" ".join(map(str,ans)))
+    return
+  b = 0
+  for i in range(n):
+    if b != a[i]:
+      ans.append(a[i])
+      b = a[i]
+      backtracking(depth+1)
+      ans.pop()
+
+backtracking(0)
