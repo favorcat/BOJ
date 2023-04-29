9b4f7a4fec59b03e1c4e16322d910dd2dc726b37
favorcat <favorcat.me@gmail.com>
2023-04-30 00:03:57 +0900

    Silver II #24445 알고리즘 수업 - 너비 우선 탐색 2
    https://www.acmicpc.net/problem/24445

---
+++ Silver/Silver II/24445.py
@@ -0,0 +1,31 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n, m, r = map(int,input().split())
+graph = [[] for _ in range(n+1)]
+visited = [0] * (n+1)
+cnt = 1
+
+for _ in range(m):
+  a, b = map(int,input().split())
+  graph[a].append(b)
+  graph[b].append(a)
+
+def bfs(v):
+  global cnt
+  q = deque([r])
+  visited[r] = 1
+  
+  while q:
+    v = q.popleft()
+    graph[v].sort(reverse=True)
+    for i in graph[v]:
+      if visited[i] == 0:
+        cnt += 1
+        visited[i] = cnt
+        q.append(i)
+
+bfs(r)
+for i in range(1, n+1):
+  print(visited[i])
