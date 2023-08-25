35e3fb947dd54eb52746afddc01ede5bc3b715aa
favorcat <favorcat.me@gmail.com>
2023-08-25 22:42:51 +0900

    Silver I #11729 하노이 탑 이동 순서
    https://www.acmicpc.net/problem/11729

---
+++ Silver/Silver I/11729.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+
+def top(n, a, b, c):
+  if n == 1:
+    print(a,c)
+  else:
+    top(n-1, a, c, b)
+    print(a,c)
+    top(n-1, b, a, c)
+
+cnt = 2 ** n - 1
+print(cnt)
+top(n, 1, 2, 3)
